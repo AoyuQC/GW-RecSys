170e28879dabc6721e62373e66823bcd8ad5aaa0
Chen Lei <leicaws@amazon.com>
2020-11-28 22:51:09 +0800

    部分中国化改造 common commit

--- docker/dkn/infer_docker_image/cn/build_and_push.sh
+++ docker/dkn/infer_docker_image/cn/build_and_push.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# FROM_ACCOUNT_ID 代表了引用的 docker image 在哪个账号下，
-# global region SM 用的账号是 763104351884
-# 中国 region SM 用的账号是 其他
-FROM_ACCOUNT_ID=763104351884
 # 这个账号指的是想把 build 出来的 image 放到哪个 账号的 ECR 下，也就是当前使用者的account，跟 aws sts get-caller-identity 返回的 account 一致
 ACCOUNT_ID=`aws sts get-caller-identity --query Account --output text`
 REGION=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/'`
@@ -10,7 +6,7 @@ REPO_NAME=gw-infer
 
 TAG=`date '+%Y%m%d%H%M%S'`
 
-$(aws ecr get-login --no-include-email --region ${REGION} --registry-ids $FROM_ACCOUNT_ID)
+docker pull tensorflow/serving
 
 docker build -f ./Dockerfile -t $REPO_NAME .
 
--- docker/dkn/infer_docker_image/cn/copy_model.py
+++ docker/dkn/infer_docker_image/cn/copy_model.py
@@ -1,12 +1,14 @@
 import boto3, sys
 
+## python copy_model.py s3://leigh-gw/dkn_model/dkn-2020-11-28-06-41-17-782/output/model.tar.gz ./
+
 s3client = boto3.client('s3')
 
 s3_key = sys.argv[1]
 path = sys.argv[2]
 
-print(s3_key)
-print(path)
+print('file on the s3 location:' + s3_key)
+print('local file path: ' + path)
 
 bucket, key = s3_key.split('/', 2)[-1].split('/', 1)
 
